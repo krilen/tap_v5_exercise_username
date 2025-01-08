3ec49201e6ad6720f98d20f1ce946e64e0ffb097
krilen <krister@lenberg.se>
2025-01-08 16:06:06 +0100

    Added exception

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main()->None:
     name:str = input("Please enter you name >> ")
-    number:int = int(input("Please enter a number >> "))
+    
+    try:
+        number:int = int(input("Please enter a number >> "))
+    # Capture ALL errors not only ValueError
+    except:
+        print("That was not a valid number, I will help you")
+        number = 0
+    
 
     print(f"Your name is {name.title()!r} and your number plus one is {str(number+1)!r}.")
 
